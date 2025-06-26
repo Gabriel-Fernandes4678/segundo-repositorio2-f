e62c7472044c97c2e021c2d11d211ffe8ebe3110
Gabriel-Fernandes4678 <gabriel.fernandes02@escola.pr.gov.br>
2025-06-26 20:59:47 -0300

    Aula 1 26/06 2

--- script..js
+++ script..js
@@ -1,6 +1,6 @@
 const botaoMostrarPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostrarPalavras.addEventListener('click', mostraPalavraChave);
+botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 
 function mostraPalavraChave() {
     alert("Fui clicado!");
